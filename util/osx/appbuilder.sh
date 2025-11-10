49e803bf3e394b6139d6bf9387f37632849b2b5c
Paolo Zago <paolo.zago@gmail.com>
2025-11-10 09:02:27 +0100

    Add DMG artifact creation step to appbuilder.sh

--- util/osx/appbuilder.sh
+++ util/osx/appbuilder.sh
@@ -195,7 +195,9 @@ then
 	codesign --force -s - "$newappname"
 fi
 
-hdiutil create -volname "Attract-Mode Plus v${SHORTVERSION}" -srcfolder "$newappname" -ov -format UDRW "$buildpath/Attract-Mode Plus v${SHORTVERSION}.dmg"
+echo STEP 8 - BUILD DMG ARTIFACT
 
+hdiutil create -volname "Attract-Mode Plus v${SHORTVERSION}" -srcfolder "$newappname" -ov -format UDRW "$buildpath/Attract-Mode Plus v${SHORTVERSION}.dmg"
+rm -R $newappname
 
 echo ALL DONE
