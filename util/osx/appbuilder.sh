89ee682e0d0556d62ac15c2adeab6099b4c584bb
Paolo Zago <paolo.zago@gmail.com>
2025-11-10 08:33:03 +0100

    Add DMG creation for Attract-Mode Plus

--- util/osx/appbuilder.sh
+++ util/osx/appbuilder.sh
@@ -195,4 +195,7 @@ then
 	codesign --force -s - "$newappname"
 fi
 
+hdiutil create -volname "Attract-Mode Plus v${SHORTVERSION}" -srcfolder "$newappname" -ov -format UDRW "Attract-Mode Plus v${SHORTVERSION}.dmg"
+
+
 echo ALL DONE
